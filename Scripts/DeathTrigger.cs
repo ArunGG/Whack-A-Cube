d2700d172fa0166b8a69017bef1d124ced251360
arunGG <46014247+arunGG@users.noreply.github.com>
2019-01-07 19:58:13 +0530

    Add files via upload

---
+++ Scripts/DeathTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour {
+
+    private SpawnCube spawnCube;
+
+
+
+	void Start ()
+    {
+
+        spawnCube = GameObject.Find("GameManager").GetComponent<SpawnCube>();
+		
+	}
+	
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            spawnCube.anim["cubeAnimation"].enabled = false;
+            //Debug.Log("GameOver");
+            Destroy(collision.collider.gameObject);
+            spawnCube.score.SetText("GAME OVER!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+
+}
---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LoadLevel()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("whackACube");
+
+
+
+    }
+
+    public void Quit()
+    {
+
+        Application.Quit();
+
+
+
+    }
+}
